94ae6fff3859c30feb1ce6a810e229bb5cc43b84
Karla Patricia Miranda Orellana <mo0436032023@unab.edu.sv>
2023-03-11 17:40:43 -0600

    Create practica_de_tutoria.py

---
+++ practica_de_tutoria.py
@@ -0,0 +1,38 @@
+#Operadores de comparacion
+'''
+num1 = 4
+num2 = 7
+
+res1 = num1 <= num2
+
+print(res1)
+'''
+#Operadores aritmeticos
+
+#Num3 += Num4
+#print(Num3)
+
+Num3 = 4
+Num4 = 2
+
+res2 = Num3 + Num4
+
+print(f"El resultado de sumar {Num3} + {Num4} = {res2}") #f-string
+
+print("El resultado de sumar", Num3, "+", Num4, "=", res2)
+
+print("El resultado de sumar {} + {} = {}".format(Num3, Num4, res2))
+
+#+, -, **, /, //, %, *
+
+Num3 = 4
+Num4 = 2
+
+res2 = Num3 % Num4
+
+print(f"El resultado del modulo {Num3} % {Num4} = {res2}") #f-string
+
+print("El resultado del modulo", Num3, "%", Num4, "=", res2)
+
+print("El resultado del modulo {} % {} = {}".format(Num3, Num4, res2))
+
